9e287089c29c90065038a5108c71eadb18555c8c
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-11-08 23:44:28 +0100

    No json serialization of App Secret for now

--- src/aguin/model/app.go
+++ src/aguin/model/app.go
@@ -3,14 +3,13 @@ package model
 import (
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
-	"time"
 )
 
 type Application struct {
 	Id          bson.ObjectId `json:"id" bson:"_id,omitempty"`
 	Userid      bson.ObjectId
 	Name        string
-	Secret      string 
+	Secret      string `json:"-"`
 	Description string
 }
 
