d63332f830ba9910a4aac55dc16892effad18cd5
Nguyen Van Nhu <nguyen.van.nhu@vnmedia.se>
2014-11-18 18:58:09 +0100

    Adding decrypt test

--- src/aguin/crypto/signed_test.go
+++ src/aguin/crypto/signed_test.go
@@ -16,4 +16,11 @@ func TestEncrypt(t *testing.T) {
 	assert.Equal(t, expected, r)
 }
 
-
+func TestDecrypt(t *testing.T) {
+	authKey := []byte("545e0716f2fea0c7a9c46c74fec46c71")
+	input := "WmtlMxl4d_VTfUYnl-A0Uycpr2e3VswKDwoPd03XtoY=.JZMk6FZloYh5BL0K7dHGSyTqB4lTgd9annrFEgLTELnxR3bHweL2"
+	expected := map[string]interface{}{"entity": "something2"}
+	r, _ := Decrypt(input, authKey, authKey)
+	assert.Equal(t, expected, r)
+	
+}
